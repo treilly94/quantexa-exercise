ef619c5f46b95cf78e2e9f340211f57965f0682d
treilly94 <treilly94@live.com>
2018-05-28 21:15:30 +0100

    wrote main and function for question 1

---
+++ src/main/scala/StatisticsGenerator.scala
@@ -0,0 +1,24 @@
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.functions.sum
+
+object StatisticsGenerator {
+  def main(args: Array[String]): Unit = {
+    // Preparation
+    val spark: SparkSession = getSparkSession("Statistics Generator")
+    val df: DataFrame = spark.read.option("header", "true").csv("./src/main/resources/transactions.txt")
+    // Question1
+    groupAndSum(df, List("transactionDay"), "transactionAmount", "totalValue").show()
+  }
+
+  def getSparkSession(name: String): SparkSession = {
+    SparkSession
+      .builder()
+      .master("local")
+      .appName(name)
+      .getOrCreate()
+  }
+
+  def groupAndSum(df: DataFrame, partCols: List[String], sumCol: String, name: String): DataFrame = {
+    df.groupBy(partCols.head, partCols.tail: _*).agg(sum(sumCol).alias(name))
+  }
+}
