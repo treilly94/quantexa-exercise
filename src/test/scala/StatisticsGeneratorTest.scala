ced261f28b1858a49fcec25d48c2a63db3948ea4
Tom Reilly <treilly94@live.com>
2018-05-29 14:29:44 +0100

    Changed test to use json to avoid having to set the types in code

--- src/test/scala/StatisticsGeneratorTest.scala
+++ src/test/scala/StatisticsGeneratorTest.scala
@@ -1,6 +1,7 @@
 import StatisticsGenerator._
 import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.scalatest.{FlatSpec, Matchers}
+import org.apache.spark.sql.functions.col
 
 class StatisticsGeneratorTest extends FlatSpec with Matchers {
 
@@ -12,23 +13,29 @@ class StatisticsGeneratorTest extends FlatSpec with Matchers {
 
   spark.sparkContext.setLogLevel("ERROR")
 
-  // TODO change test data to json to avoid type issues
-  def readCSV(path: String): DataFrame = spark.read.option("header", "true").csv(path)
+  def orderCols(df: DataFrame): DataFrame = {
+    val cols: Array[String] = df.columns.sorted
+    df.select(cols.head, cols.tail: _*)
+  }
+
+  def readJSON(path: String): DataFrame = orderCols(spark.read.json(path))
 
 
   "GroupAndSum" should "return the expected values" in {
     println("Input Data")
-    val inDf: DataFrame = readCSV("./src/test/resources/inputs/question1.csv")
+    val inDf: DataFrame = readJSON("./src/test/resources/inputs/question1.json")
     inDf.show()
     println("Expected Data")
-    val expDf: DataFrame = readCSV("./src/test/resources/expected/question1.csv")
+    val expDf: DataFrame = readJSON("./src/test/resources/expected/question1.json")
     expDf.show()
     println("Output Data")
-    val outDf: DataFrame = groupAndSum(inDf, List("transactionDay"), "transactionAmount", "totalValue")
+    val outDf: DataFrame = orderCols(
+      groupAndSum(inDf, List("transactionDay"), "transactionAmount", "totalValue")
+    )
         .orderBy("transactionDay")
     outDf.show()
 
-    outDf.collect() should be(expDf.collect())
+    outDf.collectAsList() should be(expDf.collectAsList())
   }
 
 }
