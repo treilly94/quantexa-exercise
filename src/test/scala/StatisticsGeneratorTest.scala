cdd07c12fa47b9f76613066675889f3fb7fa6c5f
Tom Reilly <treilly94@live.com>
2018-05-31 14:59:55 +0100

    Added test for question 3

--- src/test/scala/StatisticsGeneratorTest.scala
+++ src/test/scala/StatisticsGeneratorTest.scala
@@ -19,7 +19,7 @@ class StatisticsGeneratorTest extends FlatSpec with Matchers {
     groupAndSum(input) should be(expected)
   }
 
-  "groupAndMean" should "return the mean values for each account and catagory" in {
+  "groupAndMean" should "return the mean values for each account and category" in {
     val input: List[Transaction] = List(
       Transaction("1", "1", 1, "a", 10.5),
       Transaction("2", "1", 1, "a", 5.5),
@@ -35,6 +35,51 @@ class StatisticsGeneratorTest extends FlatSpec with Matchers {
       "3" -> Map("c" -> 1.0)
     )
     groupAndMean(input) should be(expected)
+  }
+
+  "lastFiveStats" should "return a valid stats object for each account and day" in {
+    val input: List[Transaction] = List(
+      Transaction("00", "10", 0, "AA", 1.00),
+      Transaction("01", "10", 1, "AA", 1.00),
+      Transaction("02", "10", 2, "AA", 1.00),
+      Transaction("03", "10", 3, "AA", 1.00),
+      Transaction("04", "10", 4, "AA", 1.00),
+      Transaction("05", "10", 5, "AA", 1.00),
+      Transaction("96", "20", 4, "BB", 1.11),
+      Transaction("06", "20", 5, "BB", 1.22),
+      Transaction("07", "20", 6, "FF", 1.33),
+      Transaction("08", "20", 7, "AA", 1.44),
+      Transaction("09", "20", 8, "CC", 1.55),
+      Transaction("10", "20", 9, "CC", 1.66),
+      Transaction("11", "30", 4, "CC", 5.00),
+      Transaction("12", "30", 5, "AA", 4.00),
+      Transaction("13", "30", 6, "FF", 3.00),
+      Transaction("14", "30", 7, "SS", 2.00),
+      Transaction("15", "30", 8, "KK", 1.00),
+      Transaction("11", "30", 9, "CC", 0.00)
+    )
+
+    val expected: List[Stats] = List(
+      Stats(0, "10", 0.00, 0.000, 0.00, 0.00, 0.00),
+      Stats(1, "10", 1.00, 1.000, 1.00, 0.00, 0.00),
+      Stats(2, "10", 1.00, 1.000, 2.00, 0.00, 0.00),
+      Stats(3, "10", 1.00, 1.000, 3.00, 0.00, 0.00),
+      Stats(4, "10", 1.00, 1.000, 4.00, 0.00, 0.00),
+      Stats(5, "10", 1.00, 1.000, 5.00, 0.00, 0.00),
+      Stats(4, "20", 0.00, 0.000, 0.00, 0.00, 0.00),
+      Stats(5, "20", 1.11, 1.110, 0.00, 0.00, 0.00),
+      Stats(6, "20", 1.22, 1.165, 0.00, 0.00, 0.00),
+      Stats(7, "20", 1.33, 1.220, 0.00, 0.00, 1.33),
+      Stats(8, "20", 1.44, 1.275, 1.44, 0.00, 1.33),
+      Stats(9, "20", 1.55, 1.330, 1.44, 1.55, 1.33),
+      Stats(4, "30", 0.00, 0.000, 0.00, 0.00, 0.00),
+      Stats(5, "30", 5.00, 5.000, 0.00, 5.00, 0.00),
+      Stats(6, "30", 5.00, 4.500, 4.00, 5.00, 0.00),
+      Stats(7, "30", 5.00, 4.000, 4.00, 5.00, 3.00),
+      Stats(8, "30", 5.00, 3.500, 4.00, 5.00, 3.00),
+      Stats(9, "30", 5.00, 3.000, 4.00, 5.00, 3.00)
+    )
+    lastFiveStats(input) should be(expected)
   }
 
 }
