a1fd0002a74240c8cbee7946f6883d58e60b8cfd
Tom Reilly <treilly94@live.com>
2018-05-31 10:48:21 +0100

    Added function for question1

--- src/main/scala/StatisticsGenerator.scala
+++ src/main/scala/StatisticsGenerator.scala
@@ -4,7 +4,10 @@ object StatisticsGenerator {
   def main(args: Array[String]): Unit = {
     // Read data
     val data: List[Transaction] = csvParser("./src/main/resources/transactions.txt")
+    println("======================Question1==============================")
     data.foreach(println)
+    println("======================Question2==============================")
+    groupAndSum(data).foreach(println)
   }
 
   def csvParser(fileName: String): List[Transaction] = {
@@ -15,6 +18,11 @@ object StatisticsGenerator {
       val split = line.split(',')
       Transaction(split(0), split(1), split(2).toInt, split(3), split(4).toDouble)
     }.toList
+  }
+
+  def groupAndSum(data: List[Transaction]): Map[Int, Double] = {
+    data.groupBy(_.transactionDay) // Generate a collection containing the transaction day and a list of all transactions under that day
+      .mapValues(_.map(_.transactionAmount).sum) // Map the transactions within each list, Take only the amount and sum them
   }
 
   case class Transaction(transactionId: String,
